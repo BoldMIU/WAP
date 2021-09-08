6070faefd2f98c2661c60c3ca21c221e31786941
Bold MIU <hi_u_baina@yahoo.com>
2021-09-07 23:18:37 -0500

    first commit

---
+++ bold.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+function gogogo() {
+    alert('Yay, it works!');
+=======
+function gogogo() {
+    alert('Yay, it works!');
+>>>>>>> f6ba7585e777a56ebf238625d0612e2afe408106
+}
---
+++ foo.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    return "Hello Bold";
+  }
+
+  document.getElementById("demo").innerHTML = myFunction();
+
+  const person = {
+   firstName: "Bold",
+   lastName: "Ganbold"
+  };
+  // alert(person.lastName + " " + person.firstName);
+
+  const person1 = {
+
+    firstName: "Dorj",
+    lastName: "Suren",
+    Age: "50"
+  };
+
+  //alert(person1.firstName + " " + person1.lastName + " " + person1.Age);
+
+
+  let txt = "We are \"Vikings\""; 
+  let x = txt.length;
+  alert(x);
